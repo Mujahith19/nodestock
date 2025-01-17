1e2cc88ac509978334fcb387b0e71b7caf0bcdc5
Mujahth <mujahith1710@gmail.com>
2025-01-17 20:05:43 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const { engine } = require ('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+// Set Handlebars Middleware
+app.engine('handlebars',engine());
+app.set('view engine', 'handlebars');
+
+const otherstuff = "hello this is otherstuff!";
+
+
+// Set Handlebars Routes
+app.get('/', function (req, res) {
+    res.render('home', {
+    	stuff: otherstuff
+    });
+});
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(PORT, () => console.log('Server Listening on port ' + PORT))
+
